df1d89befc919cd37217f75bbe38768fee401fcd
Sándor Balázs <balazssandor0301@gmail.com>
2023-09-08 14:43:57 +0200

    worked on the recursive movefinding algorithm

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
-// Don't forget:
-// bitfilter for directions - function -> determined by the value at the position on board
+// Note to self: gdb it
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,10 +27,10 @@
 	the "d" argument is used to determine which direction we
 	want to find the opposing square
 */
-#define OPPOSING_SQUARE(S, D) ADJ_SQUARES[[ADJ_SQUARES[S-1][S] - 1]][S]
+#define OPPOSING_SQUARE(S, D) ADJ_SQUARES[ (ADJ_SQUARES[S-1][D] - 1) ][D]
 
 /* copies a 4 "item" array into another */
-#define 4BYTECOPY(FROM, TO) do { for (int i = 0; i<4; ++i) { FROM[i] = TO[i]; } } while (false);
+#define FOURBYTECOPY(FROM, TO) do { for (int i = 0; i<4; ++i) { TO[i] = FROM[i]; } } while (false);
 
 /* GAME CONSTANTS */
 
@@ -51,32 +50,40 @@
 	in NW NE SW SE order.
 	if there is no square to be found in a direction, it is set to 0
 */
-const uint8_t ADJ_SQUARES[32][4] {
-		{ 0, 0, 5, 6 },
-		{ 0, 0, 6, 7 },
-		{ 0, 0, 7, 8 },
-		{ 1, 2, 9, 10 },
-		{ 2, 3, 10, 11 },
-		{ 3, 4, 11, 12 },
-		{ 5, 6, 13, 14 },
-		{ 6, 7, 14, 15 },
-		{ 7, 8, 15, 16 },
-		{ 9, 10, 17, 18 },
-		{ 10, 11, 18, 19 },
-		{ 11, 12, 19, 20 },
-		{ 13, 14, 21, 22 },
-		{ 14, 15, 22, 23 },
-		{ 15, 16, 23, 24 },
-		{ 17, 18, 25, 26 },
-		{ 18, 19, 26, 27 },
-		{ 19, 20, 27, 28 },
-		{ 21, 22, 29, 30 },
-		{ 22, 23, 30, 31 },
-		{ 23, 24, 31, 32 },
-		{ 25, 26, 0, 0 },
-		{ 26, 27, 0, 0 },
-		{ 27, 28, 0, 0 }
-}
+const uint8_t ADJ_SQUARES[32][4] = {
+		{ 0, 0, 5, 6 },     // 1
+		{ 0, 0, 6, 7 },     // 2
+		{ 0, 0, 7, 8 },     // 3
+		{ 0, 0, 8, 0 },     // 4
+		{ 0, 1, 0, 9 },     // 5
+		{ 1, 2, 9, 10 },    // 6
+		{ 2, 3, 10, 11 },   // 7
+		{ 3, 4, 11, 12 },   // 8
+		{ 5, 6, 13, 14 },   // 9
+		{ 6, 7, 14, 15 },   // 10
+		{ 7, 8, 15, 16 },   // 11
+		{ 8, 0, 16, 0 },    // 12
+		{ 0, 9, 0, 17 },    // 13
+		{ 9, 10, 17, 18 },  // 14
+		{ 10, 11, 18, 19 }, // 15
+		{ 11, 12, 19, 20 }, // 16
+		{ 13, 14, 21, 22 }, // 17
+		{ 14, 15, 22, 23 }, // 18
+		{ 15, 16, 23, 24 }, // 19
+		{ 16, 0, 24, 0 },   // 20
+		{ 0, 17, 0, 25 },   // 21
+		{ 17, 18, 25, 26 }, // 22
+		{ 18, 19, 26, 27 }, // 23
+		{ 19, 20, 27, 28 }, // 24
+		{ 21, 22, 29, 30 }, // 25
+		{ 22, 23, 30, 31 }, // 26
+		{ 23, 24, 31, 32 }, // 27
+		{ 24, 0, 32, 0 },   // 28
+		{ 0, 25, 0, 0 },    // 29
+		{ 25, 26, 0, 0 },   // 30
+		{ 26, 27, 0, 0 },   // 31
+		{ 27, 28, 0, 0 }    // 32
+};
 
 
 /* ANSI ESCAPE CODES */
@@ -115,6 +122,17 @@ move_equal(Move m1, Move m2, bool ignore_taken){
 	if (m1.from != m2.from || m1.to != m2.to)
 		return false;
 	return true;
+}
+
+/* Copies one Move into the other */
+void
+move_copy(Move m_from, Move *m_to){
+	printf("move_copy()\n");
+	m_to->from = m_from.from;
+	m_to->to   = m_from.to;
+	m_to->taken_len = m_from.taken_len;
+	for (int i = 0; i<m_from.taken_len; ++i)
+		(m_to->taken)[i] = (m_from.taken)[i];
 }
 
 /* Computer Science Moment: Storing the Moves in a Linked List */
@@ -223,7 +241,7 @@ movelist_contains(MoveList *mvlstptr, Move m, bool ignore_taken){
    true means the piece is allowed to move in that direction
 */
 void
-direction_filter(GameCtx &gmctx, uint8_t square, bool filter[4]){
+direction_filter(GameCtx *gmctx, uint8_t square, bool filter[4]){
 	char piece = (gmctx->board)[square-1];
 	
 	if (ISUPPERCASE(piece))
@@ -387,43 +405,107 @@ movelist_print(MoveList *mvlstptr) {
 /*
 	here's the meat: the --MOVES--
 */
+
 void
-available_moves(GameCtx &gmctx, uint8_t square, MoveList **mvlstptr){
+recursive_take(GameCtx *gmctx, uint8_t square, Move *m, MoveList **mvlstptr, DIRECTION d, bool dir_filter[4]){
 
+	printf("line 409: recursive_take() entered\n");
+	
 	uint8_t adj_squares[4];
-	bool dir_filter[4];
 
-	4BYTECOPY(ADJ_SQUARES[square-1], adj_squares);
+	FOURBYTECOPY(ADJ_SQUARES[square-1], adj_squares);
 
-	direction_filter(gmctx, square, dir_filter);
+	for ( int i = 0; i<4; ++i){
 
-	Move m;
+		if (!dir_filter[i]) continue;
 
-	for (int i = 0; i<4; ++i){
-		if (dir_filter[i]){
-			if ((gmctx->board)[adj_squares[i]-1] == ' '){
-				m.from = square;
-				m.to = adj_squares[i];
-				m.taken_len = 0;
-				movelist_append(mvlstptr, m);
-			}
-			else if ((gmctx->board)[adj_squares[i]-1] != (gmctx->board)[square-1]){
-				m.from = square;
-				uint8_t opp_sqr = OPPOSING_SQUARE(adj_squares[i]);
-				printf("%d\n", opp_sqr);
-				m.to = opp_sqr;
-				m.taken_len = 1;
-				m.taken[m.taken_len-1] = adj_squares[i];
-				movelist_append(mvlstptr, m);
+		uint8_t adj_square = adj_squares[i];
+
+		if (!adj_square || (gmctx->board)[adj_square-1] == ' ') return;
+
+		else if ((gmctx->board)[adj_square-1] != (gmctx->board)[square-1]){
+
+			uint8_t opp_sqr = OPPOSING_SQUARE(square, i);
+
+			if ((gmctx->board)[opp_sqr-1] == ' '){
+
+				m->taken_len += 1;
+				m->taken[(m->taken_len)-1] = adj_square;
+
+
+				if (d != i) {
+					Move new_m;
+					move_copy(*m, &new_m);
+
+					recursive_take(gmctx, opp_sqr, &new_m, mvlstptr, i, dir_filter );
+
+					new_m.to = opp_sqr;
+					movelist_append(mvlstptr, new_m);
+
+				}
+				else {
+					// we recurse (plz don't smash the stack)
+					recursive_take(gmctx, opp_sqr, m, mvlstptr, i, dir_filter);
+					
+					m->to = opp_sqr;
+					movelist_append(mvlstptr, *m);
+					
+				}
+
 			}
 		}
 	}
 }
 
 void
-do_move(char board[32], Move m){
-	board[m.to - 1] = board[m.from - 1];
-	board[m.from - 1] = ' ';
+available_moves(GameCtx *gmctx, uint8_t square, MoveList **mvlstptr){
+	
+	if ((gmctx->board)[square-1] == ' ')
+		return;
+
+	uint8_t adj_squares[4];
+	bool dir_filter[4];
+
+	FOURBYTECOPY(ADJ_SQUARES[square-1], adj_squares);
+
+	direction_filter(gmctx, square, dir_filter);
+
+	Move m;
+
+	for (int i = 0; i<4; ++i){
+
+		if (!dir_filter[i] || !(adj_squares[i])) continue;
+
+		printf("Checking direction %d\n", i);
+
+		if ((gmctx->board)[adj_squares[i]-1] == ' '){
+			m.from = square;
+			m.to = adj_squares[i];
+			m.taken_len = 0;
+			movelist_append(mvlstptr, m);
+		}
+		else if ((gmctx->board)[adj_squares[i]-1] != (gmctx->board)[square-1]){
+			uint8_t opp_sqr = OPPOSING_SQUARE(square, i);
+			if ((gmctx->board)[opp_sqr-1] == ' '){
+				m.from = square;
+				m.taken_len += 1;
+				m.taken[m.taken_len-1] = adj_squares[i];
+
+				// we recurse (plz don't smash the stack)
+				recursive_take(gmctx, opp_sqr, &m, mvlstptr, i, dir_filter);
+			}
+		}
+		for (int j = 0; j<m.taken_len; ++j)
+			m.taken[j] = 0;
+		m.taken_len = 0;
+	}
+}
+
+void
+do_move(GameCtx *gmctx, Move m){
+	movelist_append(&(gmctx->movelist), m);
+	(gmctx->board)[m.to - 1] = (gmctx->board)[m.from - 1];
+	(gmctx->board)[m.from - 1] = ' ';
 }
 
 /*
@@ -485,7 +567,7 @@ parse_cmd(GameCtx *gmctx, char cmd[6], bool *error) {
 			uint8_t square = coord_to_square(cmd[1], cmd[2]-'0');
 
 			MoveList *available = NULL;
-			available_moves( gmctx->board, square, &available);
+			available_moves( gmctx, square, &available);
 			printf("The available moves for the piece on square %d are: ", square);
 			movelist_print(available);
 			print_board_with_moves( gmctx->board, square, available);
@@ -501,7 +583,7 @@ parse_cmd(GameCtx *gmctx, char cmd[6], bool *error) {
 			if (result >= 1 && result <= 32) {
 				uint8_t square = (uint8_t)result;
 				MoveList *available = NULL;
-				available_moves( gmctx->board, square, &available);
+				available_moves( gmctx, square, &available);
 				printf("The available moves for the piece on square %d are: ", square);
 				movelist_print(available);
 				print_board_with_moves( gmctx->board, square, available);
@@ -665,7 +747,6 @@ main(int argc, char *argv[]){
 	char cmd[8];
 	
 	gmctx.movelist = NULL;
-	MoveList *available;
 
 	while (!gmctx.quit){
 		putc('>',stdout);
@@ -673,11 +754,9 @@ main(int argc, char *argv[]){
 		getchar();
 		bool error;
 		Move move = parse_cmd(&gmctx, cmd, &error);
-		if (move.from){
-			available_moves(&gmctx, move.from);
-			//movelist_append(&(gmctx.movelist), move);
-			//do_move(gmctx.board, move);
-			//print_board(gmctx.board);
+		if (!error && move.from){
+			do_move(&gmctx, move);
+			print_board(gmctx.board);
 		}
 		memset(cmd, 0, sizeof cmd);
 	}
